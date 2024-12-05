0e26de323c597d921c6bc4539e524d29933e2091
Preeyawat Inhangwa <preeyawat@jibsoft.co.th>
2024-12-05 22:49:52 +0700

    Initial project

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,11 @@
+// Dummy Cart Page
+import 'package:flutter/cupertino.dart';
+
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Cart Page'),
+    );
+  }
+}
