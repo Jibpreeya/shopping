c5d283fb7561fe733e6d4364d89403e0f46e879c
Preeyawat Inhangwa <preeyawat@jibsoft.co.th>
2024-12-09 02:49:10 +0700

    Fix product model

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -2,8 +2,14 @@ class Product {
   final int id;
   final String name;
   final int price;
+  int quantity;
 
-  Product({required this.id, required this.name, required this.price});
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    this.quantity = 1,
+  });
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
